78299997e6d54fb949f0f892857ea30e1f834b0d
sbhjt-gr <sg_outlp@outlook.com>
2025-08-06 12:05:41 +0530

    fix: header bugfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,6 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
